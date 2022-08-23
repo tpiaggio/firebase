c1a7b2ce60ffd7416e4162bdaf905c283eb581c4
juanmasilf <juanmasilveira@hotmail.com>
2022-08-23 18:20:52 -0300

    flutter - firestore

---
+++ flutter/to_do_serverless/lib/to_do_list/widget/to_do_list.dart
@@ -0,0 +1,29 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:to_do_serverless/to_do_list/logic/firestore_service.dart';
+import 'package:to_do_serverless/to_do_list/widget/task_card.dart';
+
+class ToDoList extends StatelessWidget {
+  const ToDoList({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirestoreService.listenTasks(),
+        builder: (context, snapshots) {
+          if (snapshots.hasData && snapshots.data != null) {
+            final tasks = snapshots.data!.docs;
+            return ListView.builder(
+              itemCount: tasks.length,
+              itemBuilder: (context, index) {
+                return TaskCard(
+                  task: tasks[index].data(),
+                );
+              },
+            );
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        });
+  }
+}
